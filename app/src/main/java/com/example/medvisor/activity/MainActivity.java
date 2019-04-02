4936317c06ba19d3a24d813dbb2bd71f1f738b52
ahmedayman6038 <ahmedayman6038@gmail.com>
2019-04-02 22:40:15 +0200

    ch

--- app/src/main/java/com/example/medvisor/activity/MainActivity.java
+++ app/src/main/java/com/example/medvisor/activity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.medvisor.activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -29,74 +32,35 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String MyPREFERENCES = "Login_Info" ;
+    SharedPreferences sharedpreferences;
 
-    public static final String BASE_URL = "http://ahmed6038-001-site1.btempurl.com/";
-    private static Retrofit retrofit = null;
-    private final static String API_KEY = "";
-
-    private EditText emailTxt;
-    private EditText passwordTxt;
-    private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        emailTxt = (EditText) findViewById(R.id.email);
-        passwordTxt = (EditText) findViewById(R.id.password);
-        loginBtn = (Button) findViewById(R.id.login);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userEmail = emailTxt.getText().toString();
-                String userPassword = passwordTxt.getText().toString();
-                if(!TextUtils.isEmpty(userEmail) && !TextUtils.isEmpty(userPassword)){
-                    if(Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
-                        Login user = new Login(userEmail, userPassword);
-                        loginToSystem(user);
-                    }else{
-                        Toast toast = Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                }else{
-                    Toast toast = Toast.makeText(getApplicationContext(), "Please fill all inputs", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-            }
-        });
-    }
-
-    public void loginToSystem(Login user){
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String uid = sharedpreferences.getString("UserID","");
+        Toast toast = Toast.makeText(getApplicationContext(), "Patient ID : " + uid, Toast.LENGTH_SHORT);
+        toast.show();
+        if(uid.equals("")){
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
         }
-        final PatientApi patientApi = retrofit.create(PatientApi.class);
-        Call<Patient> call = patientApi.login(user);
-        call.enqueue(new Callback<Patient>() {
-            @Override
-            public void onResponse(Call<Patient> call, Response<Patient> response) {
-                if(response.code() == 200){
-                    Patient patient = response.body();
-                    Toast toast = Toast.makeText(getApplicationContext(),Integer.toString(patient.getId()) , Toast.LENGTH_SHORT);
-                    toast.show();
-                }else if(response.code() == 404){
-                    Toast toast = Toast.makeText(getApplicationContext(), "Patient Not Founded", Toast.LENGTH_SHORT);
-                    toast.show();
-                }else{
-                    Toast toast = Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-            }
-            @Override
-            public void onFailure(Call<Patient> call, Throwable throwable) {
-                Toast toast = Toast.makeText(getApplicationContext(), "something goes wrong check internet", Toast.LENGTH_SHORT);
-                toast.show();
-                //throwable.printStackTrace();
-                Log.e("Failure ",""+throwable.getLocalizedMessage());
-            }
-        });
     }
 
+
+    public void Login(View view) {
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void Logout(View view) {
+        SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.clear();
+        editor.commit();
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
 }
